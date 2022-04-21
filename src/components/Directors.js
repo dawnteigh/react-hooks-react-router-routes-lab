741cf86dbf21a5eee94bfd2ab32a80538acdc9af
Donte Ladatto <donte279@gmail.com>
2022-04-21 14:33:12 -0500

    All tests passing

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,24 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+  <div>
+    <h1>Directors Page</h1>
+    {directors.map((d, i) => {
+      return (
+        <div key={i}>
+          <h2>Name: {d.name}</h2>
+          <p>Movies:</p>
+          <ul>
+          {d.movies.map((m, i) => {
+            return <li key={i}>{m}</li>
+          })}
+          </ul>
+        </div>
+      )
+    })}
+  </div>
+  );
 }
 
 export default Directors;
