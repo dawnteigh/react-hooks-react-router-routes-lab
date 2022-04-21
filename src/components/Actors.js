0942a8fc523a26ee0a5b168aa88ab79b8d82a39a
Donte Ladatto <donte279@gmail.com>
2022-04-21 14:32:58 -0500

    commit tree d4de13901029034985fe3d67d50a347330bb0157

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,24 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+  <div>
+    <h1>Actors Page</h1>
+    {actors.map((a, i) => {
+      return (
+        <div key={i}>
+        <h2>Name: {a.name}</h2>
+        <p>Movies:</p>
+        <ul>
+          {a.movies.map((m, i) => {
+            return (<li key={i}>{m}</li>)
+          })}
+        </ul>
+        </div>
+      )
+    })}
+    </div>
+  );
 }
 
 export default Actors;
